3061000923e0c3db5e2e5dd0e7ce98789825997d
franco@mumuki.org <franco@mumuki.org>
2016-05-12 14:30:08 -0400

    Mumuki Export on 2016-05-12 14:30:08 -0400

--- 00006_Booleanos/test.js
+++ 00006_Booleanos/test.js
@@ -11,5 +11,25 @@ describe("estaEntre", function() {
   it("estaEntre(67, 0, 100) es true", function() {
     assert(estaEntre(67, 0, 100));
   });
-  
+  it("estaEntre(2, 0, 100) es true", function() {
+    assert(estaEntre(2, 0, 100));
+  });
+})
+
+describe("estaFueraDeRango", function() {
+  it("estaFueraDeRango(10, 1, 10) es false", function() {
+    assert(!estaFueraDeRango(10, 1, 10));
+  });
+  it("estaFueraDeRango(12, 1, 10) es true", function() {
+    assert(estaFueraDeRango(12, 1, 10));
+  });
+  it("estaFueraDeRango(200, 54, 112) es true", function() {
+    assert(estaFueraDeRango(200, 54, 112));
+  });
+  it("estaFueraDeRango(67, 0, 100) es false", function() {
+    assert(!estaFueraDeRango(67, 0, 100));
+  });
+  it("estaFueraDeRango(2, 0, 100) es false", function() {
+    assert(!estaFueraDeRango(2, 0, 100));
+  });
 })
