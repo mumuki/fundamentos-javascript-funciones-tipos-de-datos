237192e2fd8ce592e5ca908bb2964d18313e7b3c
Franco Leonardo Bulgarelli <flbulgarelli@yahoo.com.ar>
2016-05-16 15:43:24 -0300

    Renombrando funcion

--- 00010_Tirando los dados/description.md
+++ 00010_Tirando los dados/description.md
@@ -1,17 +1,17 @@
 Todo muy lindo, pero estos números aleatorios no parecen ser muy útiles, dado que nos genera valores entre 0 y 1. Si queremos, por ejemplo, números enteros alteatorios, `Math.random()` no nos resuelve nuestro problema. ¿O sí?
 
-> Desafío: escribí una función `tirarLosDados` que devuelva números aleatorios entre el 1 y el 6.
+> Desafío: escribí una función `tirarDado` que devuelva números aleatorios entre el 1 y el 6.
 >
 > Por ejemplo:
 >
 > ```javascript
-> ム tirarLosDados()
+> ム tirarDado()
 > 5
-> ム tirarLosDados()
+> ム tirarDado()
 > 2
-> ム tirarLosDados()
+> ム tirarDado()
 > 2
-> ム tirarLosDados()
+> ム tirarDado()
 >```
 >
 > Recordá que si te trabás podés consultar la ayuda :smile:
--- 00010_Tirando los dados/test.js
+++ 00010_Tirando los dados/test.js
@@ -1,17 +1,17 @@
-describe("tirarLosDados", function() {
-  it("tirarLosDados() siempre devuelve números entre 1 y 6", function() {
-    assert(tirarLosDados() >= 1);
-    assert(tirarLosDados() >= 1);
-    assert(tirarLosDados() >= 1);
-    assert(tirarLosDados() >= 1);
-    assert(tirarLosDados() <= 6);
-    assert(tirarLosDados() <= 6);
-    assert(tirarLosDados() <= 6);
-    assert(tirarLosDados() <= 6);
+describe("tirarDado", function() {
+  it("tirarDado() siempre devuelve números entre 1 y 6", function() {
+    assert(tirarDado() >= 1);
+    assert(tirarDado() >= 1);
+    assert(tirarDado() >= 1);
+    assert(tirarDado() >= 1);
+    assert(tirarDado() <= 6);
+    assert(tirarDado() <= 6);
+    assert(tirarDado() <= 6);
+    assert(tirarDado() <= 6);
   });
 
-  it("tirarLosDados() genera todos los números entre 1 y 6", function() {
-    var tiradasSucesivas = new Array(100).fill(1).map(x => tirarLosDados());
+  it("tirarDado() genera todos los números entre 1 y 6", function() {
+    var tiradasSucesivas = new Array(100).fill(1).map(x => tirarDado());
     assert(tiradasSucesivas.indexOf(1) >= 0, "genera el 1");
     assert(tiradasSucesivas.indexOf(2) >= 0, "genera el 2");
     assert(tiradasSucesivas.indexOf(3) >= 0, "genera el 3");
